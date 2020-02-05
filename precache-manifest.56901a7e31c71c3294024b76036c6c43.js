7df360a67aba632c980b7931edb89d58fb4127c1
gigantor <azfaralsukor@gmail.com>
2020-02-06 00:59:32 +0800

    Updates

---
+++ precache-manifest.56901a7e31c71c3294024b76036c6c43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dca410ba4e72a73d6a852c9e0167bb7",
+    "url": "/BjakMy/index.html"
+  },
+  {
+    "revision": "1e4ae52963d05294a00f",
+    "url": "/BjakMy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e80e801eda5d5e9c6e1f",
+    "url": "/BjakMy/static/js/2.3d918192.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/BjakMy/static/js/2.3d918192.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e4ae52963d05294a00f",
+    "url": "/BjakMy/static/js/main.8a8574f0.chunk.js"
+  },
+  {
+    "revision": "f9e1f5ca4f9b657fb184",
+    "url": "/BjakMy/static/js/runtime-main.a2b72458.js"
+  }
+]);
